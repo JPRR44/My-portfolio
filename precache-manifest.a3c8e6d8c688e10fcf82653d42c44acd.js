deb25a13c7818abecae5aaa6b850e94f32b39b3e
JPRR44 <juanp.robles@hotmail.com>
2020-06-06 22:24:40 -0500

    Updates

---
+++ precache-manifest.a3c8e6d8c688e10fcf82653d42c44acd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3614cdcaf8608601ee3f39d9c85a4ba6",
+    "url": "/My-portfolio/index.html"
+  },
+  {
+    "revision": "d3ce8ac1f4d5e1fd5350",
+    "url": "/My-portfolio/static/css/2.b354c165.chunk.css"
+  },
+  {
+    "revision": "a618eb5b6208ac9fa9ef",
+    "url": "/My-portfolio/static/css/main.d407af0f.chunk.css"
+  },
+  {
+    "revision": "d3ce8ac1f4d5e1fd5350",
+    "url": "/My-portfolio/static/js/2.ca3d0986.chunk.js"
+  },
+  {
+    "revision": "effe4e92e8658ad9dfbf53e804a021fb",
+    "url": "/My-portfolio/static/js/2.ca3d0986.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a618eb5b6208ac9fa9ef",
+    "url": "/My-portfolio/static/js/main.81d78e9b.chunk.js"
+  },
+  {
+    "revision": "517b6a1a9e7ad670dd00",
+    "url": "/My-portfolio/static/js/runtime-main.98de1c0a.js"
+  },
+  {
+    "revision": "94d07585a7d23dcd88e228ec27b509a0",
+    "url": "/My-portfolio/static/media/PP.94d07585.jpeg"
+  }
+]);
